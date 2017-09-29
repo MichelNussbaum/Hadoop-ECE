2a1ed9f8f9c3682c1dbdd0a1130f460e8d8d019d
Michel <michel.nussbaum@numericable.fr>
2017-09-29 14:55:56 +0200

    TP Pivot V1

---
+++ Pivot.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Pivot {
+
+  public static class PivotMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    	String[] result = value.split(",");
+    	for (int i = 0; i < result.length() ; i++) {
+    		context.write(new IntWritable(i),new IntWritable(result[i]));
+    	}
+    }
+  }
+
+  public static class PivotReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private Text result = new Text();
+    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    	String t = "";
+    	for (IntWritable val : values) {
+    		t += val.toString() + ",";
+    	}
+    	result.set(t);
+    	context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "pivot");
+    job.setJarByClass(Pivot.class);
+    job.setMapperClass(PivotMapper.class);
+    job.setCombinerClass(PivotReducer.class);
+    job.setReducerClass(PivotReducer.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
